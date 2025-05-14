2d085bf29399b08d2ed9677a5d34f4abc7964ca4
davejefferies <david_jefferies@icloud.com>
2025-05-14 23:43:36 +1200

    Update statController.ts

--- backend/src/controllers/statController.ts
+++ backend/src/controllers/statController.ts
@@ -3,119 +3,119 @@ import { controllerWrapper } from '../utils/controllerWrapper'
 import EventRepository from '../repository/eventRepository'
 
 const eventRepository = new EventRepository()
+const periods = 4
+
+async function splitArrayByPeriod(eventId: number) {
+    const logItems = await eventRepository.fetchEventLogByEvent(eventId)
+    const periods = logItems.reduce((acc: any, curr: any) => {
+        if (curr.code.code === 'GE')
+            acc.push([])
+        else
+            acc[acc.length - 1].push(curr)
+        return acc
+    }, [[]])
+
+    return periods
+}
 
 export const fetchScoringStats = controllerWrapper(async (request: Request) => {
     const eventId: number = Number(request.params.eventId || 0)
-    const logItems = await eventRepository.fetchEventLogByEvent(eventId)
-    const periods = 4
-    const points = logItems.filter((item: any) => item.code.code === 'P')
-    const oppositionPoints = logItems.filter((item: any) => item.code.code === 'PO')
+    const items: any = await splitArrayByPeriod(eventId)
     return {
-        headers: ['Name', ...Array.from({ length: periods }).map((p: any, i: number) => 'Q' + (i + 1)), 'Total'],
+        headers: [
+            'Name', 
+            ...Array.from({ length: items.length }).map((p: any, i: number) => 'Q' + (i + 1)), 
+            'Total'
+        ],
         rows: [
             [
                 'Us',
-                ...Array.from({ length: periods }).map((p: any, i: number) => points.filter((point: any) => point.period === i + 1).length),
-                points.length
+                ...Array.from({ length: items.length }).map((P: any, i: number) => items[i].filter((item: any) => item.code.code === 'P').length),
+                Array.from({ length: items.length }).map((P: any, i: number) => items[i].filter((item: any) => item.code.code === 'P').length).reduce((acc, val) => acc + val, 0)
             ],
             [
                 'Them',
-                ...Array.from({ length: periods }).map((p: any, i: number) => oppositionPoints.filter((point: any) => point.period === i + 1).length),
-                oppositionPoints.length
+                ...Array.from({ length: items.length }).map((P: any, i: number) => items[i].filter((item: any) => item.code.code === 'PO').length),
+                Array.from({ length: items.length }).map((P: any, i: number) => items[i].filter((item: any) => item.code.code === 'PO').length).reduce((acc, val) => acc + val, 0)
             ]
         ]
     }
 })
 
 export const fetchShootingStats = controllerWrapper(async (request: Request) => {
-    const result: any = {
-        headers: [],
-        rows: []
-    }
     const eventId: number = Number(request.params.eventId || 0)
-    const logItems = await eventRepository.fetchEventLogByEvent(eventId)
-    const shooters = logItems.filter((item: any) => item.code.code === 'PS' && (item.position.code === 'GS' || item.position.code === 'GA'))
+    const items: any = await splitArrayByPeriod(eventId)
+    const rows: any = []
+    const shooters = Array.from({ length: items.length }).map((P: any, i: number) => 
+        items[i].filter((item: any) => item.code.code === 'PS' && (item.position.code === 'GS' || item.position.code === 'GA'))).flat()
     .reduce((unique: any[], item: any) => {
         const alreadyAdded = unique.some(d => d.player.id === item.player.id)
         if (!alreadyAdded) unique.push(item)
         return unique
     }, [])
-    const points = logItems.filter((item: any) => item.code.code === 'P')
-    const miss = logItems.filter((item: any) => item.code.code === 'M')
-    const periods = 4
-    result.headers = ['Name', ...Array.from({ length: periods }).map((p: any, i: number) => 'Q' + (i + 1)), 'Total']
+    
     shooters.forEach((shooter: any) => {
-        const pers: any = []
-        for (let i = 1; i <= periods; i++) {
-            const goals = points.filter(point => point.playerId === shooter.playerId && point.period === i).length
-            const misses = miss.filter(miss => miss.playerId === shooter.playerId && miss.period === i).length
-            pers.push({
-                period: i,
-                goals,
-                misses,
-                total: goals + misses,
-                percentage: goals ? ((goals / (goals + misses)) * 100) + '%' : '0%'
-            })
+        const line: string[] = [
+            shooter.player.name
+        ]
+        const totals = {
+            goals: 0,
+            missed: 0
         }
-        const goals = pers.reduce((total: number, p: any) => total + p.goals, 0)
-        const misses = pers.reduce((total: number, p: any) => total + p.misses, 0)
-        result.rows.push([
-            shooter.player.name,
-            ...Array.from({ length: periods }).map((p: any, index: number) => {
-                let found = pers.find((period: any) => period.period === index + 1)
-                if (found)
-                    return `${found.goals}/${found.total} (${found.percentage})`
-                return `0/0 (0%)`
-            }),
-            goals ? `${goals}/${(goals + misses)} (${((goals / (goals + misses)) * 100)}%)` : '0%'
-        ])
+        items.forEach((period: any) => {
+            const goals = period.filter((p: any) => p.code.code === 'P' && p.playerId === shooter.playerId).length
+            const missed = period.filter((p: any) => p.code.code === 'M' && p.playerId === shooter.playerId).length
+            totals.goals = totals.goals + goals
+            totals.missed = totals.missed + missed
+            line.push(`${goals}/${goals + missed} (${goals ? (goals / (goals + missed)) * 100 : 0}%)`)
+        })
+        line.push(`${totals.goals}/${totals.goals + totals.missed} (${totals.goals ? (totals.goals / (totals.goals + totals.missed)) * 100 : 0}%)`)
+        rows.push(line)
     })
-    return result
+
+    return {
+        headers: [
+            'Name', 
+            ...Array.from({ length: items.length }).map((p: any, i: number) => 'Q' + (i + 1)), 
+            'Total'
+        ],
+        rows
+    }
 })
 
 export const fetchReboundStats = controllerWrapper(async (request: Request) => {
-    const periods = 4
     const eventId: number = Number(request.params.eventId || 0)
-    const logItems = await eventRepository.fetchEventLogByEvent(eventId)
-    const defenders = logItems.filter((item: any) => item.code.code === 'PS' && (item.position.code === 'GD' || item.position.code === 'GK'))
-    .reduce((unique: any[], item: any) => {
-        const alreadyAdded = unique.some(d => d.player.id === item.player.id)
-        if (!alreadyAdded) unique.push(item)
-        return unique
-    }, [])
-    const result: any = {
-        headers: ['Name', ...Array.from({ length: periods }).map((p: any, i: number) => 'Q' + (i + 1)), 'Total'],
-        rows: []
-    }
-    const gained = logItems.filter((item: any) => item.code.code === 'R')
-    const lost = logItems.filter((item: any) => item.code.code === 'LR')
+    const items: any = await splitArrayByPeriod(eventId)
+    const rows: any = []
+    const defenders = Array.from({ length: items.length }).map((P: any, i: number) => 
+        items[i].filter((item: any) => item.code.code === 'PS' && (item.position.code === 'GK' || item.position.code === 'GD'))).flat()
 
     defenders.forEach((defender: any) => {
-        const pers: any = []
-        for (let i = 1; i <= periods; i++) {
-            const gains = gained.filter(gain => gain.playerId === defender.playerId && gain.period === i).length
-            const losses = lost.filter(loss => loss.playerId === defender.playerId && loss.period === i).length
-            pers.push({
-                period: i,
-                gains,
-                losses,
-                total: gains + losses,
-                percentage: gains ? ((gains / (gains + losses)) * 100) + '%' : '0%'
-            })
+        const line: string[] = [
+            defender.player.name
+        ]
+        const totals = {
+            gains: 0,
+            loses: 0
         }
-        const totalGains = pers.reduce((total: number, p: any) => total + p.gains, 0)
-        const totalLosses = pers.reduce((total: number, p: any) => total + p.losses, 0)
-        result.rows.push([
-            defender.player.name,
-            ...Array.from({ length: periods }).map((p: any, index: number) => {
-                let found = pers.find((period: any) => period.period === index + 1)
-                if (found)
-                    return `${found.gains}/${found.total} (${found.percentage})`
-                return `0/0 (0%)`
-            }),
-            totalGains ? `${totalGains}/${(totalGains + totalLosses)} (${((totalGains / (totalGains + totalLosses)) * 100)}%)` : '0%'
-        ])
+
+        items.forEach((period: any) => {
+            const gains = period.filter((p: any) => p.code.code === 'R' && p.playerId === defender.playerId).length
+            const loses = period.filter((p: any) => p.code.code === 'LR' && p.playerId === defender.playerId).length
+            totals.gains = totals.gains + gains
+            totals.loses = totals.loses + loses
+            line.push(`${gains}/${gains + loses} (${gains ? (gains / (gains + loses)) * 100 : 0}%)`)
+        })
+        line.push(`${totals.gains}/${totals.gains + totals.loses} (${totals.gains ? (totals.gains / (totals.gains + totals.loses)) * 100 : 0}%)`)
+        rows.push(line)
     })
 
-    return result
+    return {
+        headers: [
+            'Name', 
+            ...Array.from({ length: items.length }).map((p: any, i: number) => 'Q' + (i + 1)), 
+            'Total'
+        ],
+        rows
+    }
 })
